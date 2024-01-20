9da2ac58ff437a2871d1756694da57bb1da86462
Mateusz K <code4well@gmail.com>
2024-01-20 15:27:01 +0100

    SQL usefull code - IntiCap and SplitString functions

---
+++ SQL/UsefullCode/fnSplitString.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION fnSplitString (
+  @String NVARCHAR(max),
+  @Delimiter NVARCHAR(255),
+	@ReturnIndex INT = 1
+)
+RETURNS NVARCHAR(MAX)
+AS
+	BEGIN
+		
+		-- Żeby otrzymać prawidłowy split musimy sprawdzić, czy ten separator nie występuje gdzieś w zbiorze danych
+		DECLARE @Separator NVARCHAR(1) = '〞'
+		DECLARE @StringWithSeparator NVARCHAR(MAX) = REPLACE(@String, @Delimiter, @Separator)
+
+		RETURN (
+			SELECT
+				value
+			FROM
+				string_split(@StringWithSeparator, @Separator, 1)
+			WHERE
+				ordinal = @ReturnIndex
+		)
+
+	END
+GO
