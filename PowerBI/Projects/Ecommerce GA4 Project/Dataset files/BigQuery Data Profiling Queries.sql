043936d9ecdbb1fa646229b842c0e4786e20c5b6
Mateusz K <code4well@gmail.com>
2024-02-26 22:13:39 +0100

    Ecommerce GA4 Project - BigQuery Data Profiling (Customer Behavior, Traffic Sources)

--- PowerBI/Projects/Ecommerce GA4 Project/Dataset files/BigQuery Data Profiling Queries.sql
+++ PowerBI/Projects/Ecommerce GA4 Project/Dataset files/BigQuery Data Profiling Queries.sql
@@ -106,7 +106,8 @@ GROUP BY
   i.item_name
 ORDER BY 
   revenue_by_item desc
-LIMIT 10;
+LIMIT 
+  10;
 
 
 
@@ -150,7 +151,7 @@ GROUP BY
 
 -- Conversion Funnel Analysis
 -- Calculation: Percentage of users who complete each stage of the conversion funnel.
-WITH conversion_funnel AS (
+WITH conversion_funnel as (
   SELECT
     count(distinct case when event_name = 'session_start' then ep.value.int_value end) as sessions_count
     , count(distinct case when event_name = 'view_item' then ep.value.int_value end) as product_views_count
@@ -181,7 +182,7 @@ FROM
 
 -- Average Session Duration
 -- Calculation: Average duration of sessions.
-WITH event_duration AS (
+WITH event_duration as (
   SELECT
     concat(user_pseudo_id, cast(event_timestamp as string)) user_id_event_timestamp
     , max(if(ep.key = 'ga_session_id', ep.value.int_value, null)) as ga_session_id
@@ -194,8 +195,8 @@ WITH event_duration AS (
     or ep.key = 'engagement_time_msec'
   GROUP BY
     user_id_event_timestamp
-),
-session_duration AS (
+)
+, session_duration as (
   SELECT
     ga_session_id
     , sum(engagement_time_msec) / 1000 session_duration_seconds
@@ -212,7 +213,7 @@ FROM
 
 -- Pages Per Session
 -- Calculation: Average number of pages viewed per session.
-WITH session_page_viewed AS (
+WITH session_page_viewed as (
   SELECT
     ep.value.int_value as ga_session_id
     , count(event_name) as page_viewed
@@ -229,6 +230,153 @@ SELECT
   round(avg(page_viewed)) as avg_pages_per_session
 FROM
   session_page_viewed
+
+
+-- Bounce Rate
+-- Calculation: Percentage of single-page sessions.
+WITH sessions_with_hits as (
+  SELECT
+    ep.value.int_value as ga_session_id
+  FROM 
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+    , unnest(event_params) as ep
+  WHERE
+    ep.key = 'ga_session_id'
+    and event_name = 'page_view'
+  GROUP BY
+    ga_session_id
+  HAVING
+    count(*) = 1
+)
+, sessions_count as (
+  SELECT
+    count(distinct ep.value.int_value) as sessions_count
+  FROM 
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+    , unnest(event_params) as ep
+  WHERE
+    ep.key = 'ga_session_id'
+)
+SELECT
+  count(distinct swh.ga_session_id) / max(sc.sessions_count) * 100 as bounce_rate
+FROM
+  sessions_with_hits as swh
+  CROSS JOIN sessions_count as sc;
+
+
+-- Top Viewed Pages
+-- Calculation: Most frequently visited pages.
+SELECT
+  ep.value.string_value as page_title
+  , count(*) as page_visits
+FROM 
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  , unnest(event_params) as ep
+WHERE
+  ep.key = 'page_title'
+  and event_name = 'page_view'
+GROUP BY
+  page_title
+ORDER BY
+  page_visits desc
+LIMIT 
+  10;
+
+
+-- User Retention Rate
+-- Calculation: Percentage of users who return to the platform over time.
+WITH user_activity as (
+  SELECT
+    user_pseudo_id,
+    min(parse_date('%Y%m%d', event_date)) as first_activity_date,
+    max(parse_date('%Y%m%d', event_date)) as last_activity_date
+  FROM
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  GROUP BY
+    user_pseudo_id
+)
+SELECT
+  countif(date_diff(last_activity_date, first_activity_date, day) >= 7)
+  / count(distinct user_pseudo_id) * 100 as user_retention_rate
+FROM
+  user_activity;
+
+
+
+-- -----[   Metrics for Traffic Sources Analysis   ]-----
+
+
+-- Total Sessions by Source/Medium
+-- Calculation: Count of sessions grouped by traffic source and medium.
+SELECT
+  traffic_source.name as source
+  , traffic_source.medium as medium
+  , count(distinct ep.value.int_value) as sessions_count
+FROM 
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  , unnest(event_params) as ep
+WHERE
+  ep.key = 'ga_session_id'
+GROUP BY
+  source
+  , medium;
+
+
+-- New vs. Returning Visitors
+-- Calculation: Count of new and returning visitors.
+WITH user_visit_count as (
+  SELECT
+    user_pseudo_id
+    , count(distinct ep.value.int_value) as visit_count
+    , case
+      when count(distinct ep.value.int_value) = 1 then 'New Visitor'
+      else 'Returning Visitor'
+    end as visitor_type
+  FROM 
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+    , unnest(event_params) as ep
+  WHERE
+    ep.key = 'ga_session_id'
+    and event_name = 'first_visit'
+  GROUP BY
+    user_pseudo_id
+)
+SELECT
+  uvc.visitor_type
+  , count(*) as visitor_count
+FROM
+  user_visit_count as uvc
+GROUP BY
+  uvc.visitor_type;
+
+
+-- Traffic Source Trends Over Time
+-- Calculation: Count of sessions over time grouped by traffic source.
+SELECT
+  traffic_source.name as source
+  , parse_date('%Y%m%d', event_date) as date
+  , count(distinct ep.value.int_value) as sessions_count
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  , unnest(event_params) as ep
+WHERE
+  event_name = 'session_start'
+  and ep.key = 'ga_session_id'
+GROUP BY
+  source
+  , date
+ORDER BY
+  date;
+
+
+
+-- -----[   Metrics for Product Performance Analysis   ]-----
+
+
+-- Total Sales by Product
+-- Calculation: Count of distinct transactions per product.
+
+
 
 
 
